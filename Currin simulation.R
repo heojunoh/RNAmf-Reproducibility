83fea051d919e2f600cf6e7686c22f72c13369bc
Junoh Heo <99292199+heojunoh@users.noreply.github.com>
2023-09-16 23:31:57 -0400

    Update Currin simulation.R

--- Currin simulation.R
+++ Currin simulation.R
@@ -4,6 +4,7 @@ library(laGP)
 library(plgp)
 library(MuFiCokriging)
 library(RNAmf)
+library(reticulate)
 
 crps <- function(x, mu, sig2){ # The smaller, the better (0 to infinity)
   if(any(sig2==0)) sig2[sig2==0] <- eps
@@ -48,10 +49,9 @@ rep <- 100
 result.currin.rmse <- matrix(NA, rep, 2)
 result.currin.meancrps <- matrix(NA, rep, 2)
 result.currin.comptime <- matrix(NA, rep, 2)
-colnames(result.currin.rmse) <- c("closed", "Cokriging")
-colnames(result.currin.meancrps) <- c("closed", "Cokriging") # The smaller, the better
-colnames(result.currin.comptime) <- c("closed", "Cokriging") # The smaller, the better
-
+colnames(result.currin.rmse) <- c("RNAmf", "Cokriging")
+colnames(result.currin.meancrps) <- c("RNAmf", "Cokriging") 
+colnames(result.currin.comptime) <- c("RNAmf", "Cokriging") 
 
 for(i in 1:rep) {
   set.seed(i)
@@ -67,21 +67,20 @@ for(i in 1:rep) {
   
   y1 <- apply(X1,1,curretal88explc)
   y2 <- apply(X2,1,curretal88exp)
+
   
   eps <- sqrt(.Machine$double.eps)
   
   ### test data ###
   x <- maximinLHS(1000, d)
   
-  
-  ### closed ###
-  tic.closed <- proc.time()[3]
-  fit.closed <- RNAmf(X1, y1, X2, y2, kernel="sqex", constant=TRUE)
-  pred.closed <- predRNAmf(fit.closed, x)
-  predy <- pred.closed$mu
-  predsig2 <- pred.closed$sig2
-  toc.closed <- proc.time()[3]
-  
+  ### RNAmf ###
+  tic.RNAmf <- proc.time()[3]
+  fit.RNAmf <- RNAmf(X1, y1, X2, y2, kernel="sqex", constant=TRUE)
+  pred.RNAmf <- predRNAmf(fit.RNAmf, x)
+  predy <- pred.RNAmf$mu
+  predsig2 <- pred.RNAmf$sig2
+  toc.RNAmf <- proc.time()[3]
   
   ### Cokriging ###
   tic.cokm <- proc.time()[3]
@@ -93,33 +92,29 @@ for(i in 1:rep) {
   toc.cokm <- proc.time()[3]
   
   
-  ### RMSE ###
+  result.currin.rmse[i,1] <- sqrt(mean((predy-apply(x,1,curretal88exp))^2)) 
+  result.currin.rmse[i,2] <- sqrt(mean((pred.muficokm$mean-apply(x,1,curretal88exp))^2)) 
   
-  result.currin.rmse[i,1] <- sqrt(mean((predy-apply(x,1,curretal88exp))^2)) # closed form
-  result.currin.rmse[i,2] <- sqrt(mean((pred.muficokm$mean-apply(x,1,curretal88exp))^2)) # Cokriging
-  
-  result.currin.meancrps[i,1] <- mean(crps(apply(x,1,curretal88exp), predy, predsig2)) # closed form
-  result.currin.meancrps[i,2] <- mean(crps(apply(x,1,curretal88exp), pred.muficokm$mean, pred.muficokm$sig2)) # Cokriging
+  result.currin.meancrps[i,1] <- mean(crps(apply(x,1,curretal88exp), predy, predsig2)) 
+  result.currin.meancrps[i,2] <- mean(crps(apply(x,1,curretal88exp), pred.muficokm$mean, pred.muficokm$sig2)) 
   
   result.currin.comptime[i,1] <- toc.closed - tic.closed
   result.currin.comptime[i,2] <- toc.cokm - tic.cokm
-  
 }
 
-install.packages("reticulate")
-library(reticulate)
+
 py_run_file("Currin.py")
 result.currin.rmse <- cbind(result.currin.rmse, NARGP=unlist(py$l2error))
 result.currin.meancrps <- cbind(result.currin.meancrps, NARGP=unlist(py$meancrps))
 result.currin.comptime <- cbind(result.currin.comptime, NARGP=unlist(py$comptime))
 
 par(mfrow=c(1,1))
-#RMSE comparison#
+### RMSE comparison ###
 apply(result.currin.rmse, 2, mean)
 table(apply(result.currin.rmse, 1, which.min))
 boxplot(result.currin.rmse)
 
-#CRPS comparison, The smaller, the better
+### CRPS comparison ###
 apply(result.currin.meancrps, 2, mean)
 table(apply(result.currin.meancrps, 1, which.min))
 boxplot(result.currin.meancrps)
