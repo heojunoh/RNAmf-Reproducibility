44fead545749afdae46c7ead779442faa6ad5cb1
Junoh Heo <99292199+heojunoh@users.noreply.github.com>
2023-09-19 18:42:08 -0400

    Update ALC_two_level.R

--- ALC_two_level.R
+++ ALC_two_level.R
@@ -414,7 +414,6 @@ ALC_two_level <- function(Xref=NULL, fit, mc.sample=100, cost, funcs, n.start, p
 
   ### Update the model ###
   newx <- matrix(chosen$Xnext, nrow=1)
-  # if(newx[1,1]==0) newx[1,1] <- sqrt(.Machine$double.eps) # To prevent yielding infinity for Park function
   level <- chosen$level
 
   X1 <- t(t(fit1$X)*attr(fit1$X,"scaled:scale")+attr(fit1$X,"scaled:center"))
