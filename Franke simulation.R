e58fbafedf1998847c8ecc1827930ed0b7d45963
Junoh Heo <99292199+heojunoh@users.noreply.github.com>
2023-09-19 18:15:26 -0400

    Update Franke simulation.R

--- Franke simulation.R
+++ Franke simulation.R
@@ -1,11 +1,4 @@
 ### Franke Example ###
-library(lhs)
-library(laGP)
-library(plgp)
-library(MuFiCokriging)
-library(RNAmf)
-library(reticulate)
-
 crps <- function(x, mu, sig2){ # The smaller, the better (0 to infinity)
   if(any(sig2==0)) sig2[sig2==0] <- eps
   -sqrt(sig2)*(1/sqrt(pi)-2*dnorm((x-mu)/sqrt(sig2))-(x-mu)/sqrt(sig2)*(2*pnorm((x-mu)/sqrt(sig2))-1))
@@ -104,8 +97,12 @@ for(i in 1:rep) {
   result.Franke.comptime[i,2] <- toc.cokm - tic.cokm
 }
 
-
-py_run_file("Franke.py")
+py_install("GPy")
+py_install("pandas")
+py_install("matplotlib")
+py_install("scipy")
+py_install("time")
+py_run_file("python code/Franke.py")
 result.Franke.rmse <- cbind(result.Franke.rmse, NARGP=unlist(py$l2error))
 result.Franke.meancrps <- cbind(result.Franke.meancrps, NARGP=unlist(py$meancrps))
 result.Franke.comptime <- cbind(result.Franke.comptime, NARGP=unlist(py$comptime))
