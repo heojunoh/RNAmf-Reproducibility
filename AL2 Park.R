96f44cb53b5ffa3c853c5958826bec0c98b13335
Junoh Heo <99292199+heojunoh@users.noreply.github.com>
2023-09-20 14:43:43 -0400

    Update AL2 Park.R

--- AL2 Park.R
+++ AL2 Park.R
@@ -1,9 +1,3 @@
-eps <- sqrt(.Machine$double.eps)
-crps <- function(x, mu, sig2){ # The smaller, the better (0 to infinity)
-  if(any(sig2==0)) sig2[sig2==0] <- eps
-  -sqrt(sig2)*(1/sqrt(pi)-2*dnorm((x-mu)/sqrt(sig2))-(x-mu)/sqrt(sig2)*(2*pnorm((x-mu)/sqrt(sig2))-1))
-}
-
 costmatc2 <- list(NA)
 rmsematc2 <- list(NA)
 crpsmatc2 <- list(NA)
