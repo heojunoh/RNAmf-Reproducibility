9ddfa6ad89a184c4a32d27bb84b7573759e536c5
Junoh Heo <99292199+heojunoh@users.noreply.github.com>
2023-09-20 14:44:10 -0400

    Update and rename AL3 Park.R to ALMC Park.R

--- ALMC Park.R
+++ ALMC Park.R
@@ -1,9 +1,3 @@
-eps <- sqrt(.Machine$double.eps)
-crps <- function(x, mu, sig2){ # The smaller, the better (0 to infinity)
-  if(any(sig2==0)) sig2[sig2==0] <- eps
-  -sqrt(sig2)*(1/sqrt(pi)-2*dnorm((x-mu)/sqrt(sig2))-(x-mu)/sqrt(sig2)*(2*pnorm((x-mu)/sqrt(sig2))-1))
-}
-
 costmatc3 <- list(NA)
 rmsematc3 <- list(NA)
 crpsmatc3 <- list(NA)
