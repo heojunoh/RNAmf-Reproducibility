8ea6e4ba28ec5ed2932e063daed9f503ef529b1b
Junoh Heo <99292199+heojunoh@users.noreply.github.com>
2023-09-20 15:48:09 -0400

    Update Borehole.py

--- python code/Borehole.py
+++ python code/Borehole.py
@@ -1,6 +1,16 @@
+import GPy
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.mlab as ml
+import matplotlib.patches as mpatches
+import scipy.stats as stats
+import rpy2.robjects as robjects
+
+import time
+
 ''' load data '''
 r = robjects.r
-loaded_data = r.readRDS("/tmp_data_borehole.rds")
+loaded_data = r.readRDS("tmp_data_borehole.rds")
 X1=np.array(loaded_data[0])
 X2=np.array(loaded_data[1])
 Y1=np.array(loaded_data[2])[:,None]
