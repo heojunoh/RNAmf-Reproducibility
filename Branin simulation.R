9abf0ccc08abb433d8217d879bf3f6bbd49110ed
Junoh Heo <99292199+heojunoh@users.noreply.github.com>
2023-09-20 15:48:27 -0400

    Update Branin simulation.R

--- Branin simulation.R
+++ Branin simulation.R
@@ -79,7 +79,7 @@ for(i in 1:rep) {
   ### test data ###
   x <- maximinLHS(1000, d)
   
-  saveRDS(list(X1=X1, X2=X2, X3=X3, Y1=y1, Y2=y2, Y3=y3, Xtest=x, Ytest=apply(x,1,output.branin)), file = "/Users/junoh/Downloads/tmp_data_branin.rds")
+  saveRDS(list(X1=X1, X2=X2, X3=X3, Y1=y1, Y2=y2, Y3=y3, Xtest=x, Ytest=apply(x,1,output.branin)), file = "tmp_data_branin.rds")
   
   ### RNAmf ###
   tic.RNAmf <- proc.time()[3]
