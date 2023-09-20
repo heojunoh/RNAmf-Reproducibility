eaef1e40b45b9e8a1271165821a806759398aeec
Junoh Heo <99292199+heojunoh@users.noreply.github.com>
2023-09-19 22:36:32 -0400

    Add files via upload

---
+++ crps.R
@@ -0,0 +1,4 @@
+crps <- function(x, mu, sig2){ # The smaller, the better (0 to infinity)
+  if(any(sig2==0)) sig2[sig2==0] <- eps
+  -sqrt(sig2)*(1/sqrt(pi)-2*dnorm((x-mu)/sqrt(sig2))-(x-mu)/sqrt(sig2)*(2*pnorm((x-mu)/sqrt(sig2))-1))
+}
