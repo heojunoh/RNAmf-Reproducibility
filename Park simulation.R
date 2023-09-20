2cf5dc3a8db62e201612eee048e7b891d36da4ef
Junoh Heo <99292199+heojunoh@users.noreply.github.com>
2023-09-20 15:55:00 -0400

    Update Park simulation.R

--- Park simulation.R
+++ Park simulation.R
@@ -93,7 +93,7 @@ for(i in 1:rep) {
   
   
   ### NARGP ###
-  py <- py_run_file("/python code/park.py")
+  py <- py_run_file("python code/park.py")
   
   result.park.rmse[i,1] <- sqrt(mean((predy-apply(x,1,park91a))^2)) 
   result.park.rmse[i,2] <- sqrt(mean((pred.muficokm$mean-apply(x,1,park91a))^2)) 
